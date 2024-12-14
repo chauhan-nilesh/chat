c0e562c7d9db8ee09d2e8923e9b1ecc772f68a7a
Nilesh Chauhan <nileshchauhan3998@gmail.com>
2024-12-14 21:06:14 +0530

    Updated app.js

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,7 +9,7 @@ import ChatMobile from "./components/ChatWindow/ChatMobile";
 import Loader from "./components/Loader";
 import BottomNavbar from "./components/BottomNavbar";
 
-const socket = io(`${import.meta.env.VITE_API_URL}`);
+const socket = io(`${import.meta.env.VITE_API_URL}`)
 
 function App() {
   const [userData, setUserData] = useState({});
